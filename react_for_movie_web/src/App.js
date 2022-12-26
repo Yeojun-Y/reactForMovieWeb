33223602ff08a56fac6744749ce15366c5e4e574
Yeojun-Y <ikm3256@naver.com>
2022-12-26 13:52:54 +0900

    0.7.1.TODO

--- react_for_movie_web/src/App.js
+++ react_for_movie_web/src/App.js
@@ -1,20 +1,35 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("Look");
-    return () => console.log("destroyed :(");
-  }, []);
-  return <h1>Hello~</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange1 = (e) => setToDo(e.target.value);
+  const onSubmit1 = (event) => {
+    event.preventDefault(); //page refresh prevent
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>MyTODO({toDos.length})</h1>
+      <form onSubmit={onSubmit1}>
+        <input
+          onChange={onChange1}
+          value={toDo}
+          type="text"
+          placeholder="Write your TODO"
+        ></input>
+        <button>AddToDo</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
